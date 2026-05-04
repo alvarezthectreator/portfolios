17750039527b6b349b45cc1e02a8071fc1bc9f6a
Wagwula George <wagwulageorge@gmail.com>
2026-05-04 15:05:15 +0100

    Update vite.config.ts for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: "/portfolios/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
